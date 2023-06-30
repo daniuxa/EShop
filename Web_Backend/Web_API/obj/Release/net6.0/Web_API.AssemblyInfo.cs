12ae2ab3e4e8f9f9026c404bef79dcdfa491f39a
daniuxa <daniakroos8@gmail.com>
2023-06-30 14:43:43 +0300

    Publish

--- Web_Backend/Web_API/obj/Release/net6.0/Web_API.AssemblyInfo.cs
+++ Web_Backend/Web_API/obj/Release/net6.0/Web_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("85b1bd65-54cb-42de-bcab-a724c606d9ff")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_API")]
